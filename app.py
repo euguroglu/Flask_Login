76edc02548a78cbbb265c3eeff65759426b8f9ab
euguroglu <e.uguroglu@yahoo.com>
2020-12-26 20:05:00 +0300

    basic set-up completed

--- app.py
+++ app.py
@@ -1,10 +1,24 @@
 from flask import Flask
-from flask_login import LoginManager
+from flask_login import LoginManager, UserMixin
+from flask_sqlalchemy import SQLAlchemy
+import os
+
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'Secret'
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///'+os.path.join(basedir,'data.sqlite')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 login_manager = LoginManager(app)
+db = SQLAlchemy(app)
+
+
+class User(UserMixin,db.Model):
+    id = db.Column(db.Integer,primary_key=True)
+    username = db.Column(db.String(30),unique=True)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
+    db.create_all()
