8a2376537d944e85ecf7e504b9ceafe7bfe7ea5b
euguroglu <e.uguroglu@yahoo.com>
2020-12-26 20:21:12 +0300

    login and user information added

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from flask_login import LoginManager, UserMixin
+from flask_login import LoginManager, UserMixin, login_user, login_required, current_user
 from flask_sqlalchemy import SQLAlchemy
 import os
 
@@ -18,6 +18,21 @@ class User(UserMixin,db.Model):
     id = db.Column(db.Integer,primary_key=True)
     username = db.Column(db.String(30),unique=True)
 
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+#or return User.query.filter_by(id=int(user_d))
+
+@app.route('/login')
+def login():
+    user = User.query.filter_by(username='enes').first()
+    login_user(user)
+    return '<h1>Logged in</h1>'
+
+@app.route('/home')
+@login_required
+def home():
+    return '<h1>You are in the protected area, {}</h1>'.format(current_user.username)
 
 if __name__ == "__main__":
     app.run(debug=True)
